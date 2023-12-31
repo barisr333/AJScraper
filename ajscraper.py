c00434bbd77bfbc5dd65252c52d7022bb84c8b10
barisr333 <baranvz@gmail.com>
2023-12-31 13:44:08 +0200

    Added flow up to job list

--- ajscraper.py
+++ ajscraper.py
@@ -1,21 +1,29 @@
 import time
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from dotenv import load_dotenv
 
-load_dotenv()
 options = webdriver.ChromeOptions()
 options.add_argument('ignore-certificate-errors')
 driver = webdriver.Chrome(options)
 try:
-    wait = WebDriverWait(driver, 30)
+    wait = WebDriverWait(driver, 10)
     action = ActionChains(driver)
     driver.get('https://www.alljobs.co.il')
-    
 
+    # Field selection
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, "//div[contains(text(), 'בחרו תחום')]")
+    )).click()
+
+    # Select 'Software'
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//div[@id=235]')
+    )).click()
+
+    driver.find_element(By.XPATH, "//button[@id='CategorySearch']").click()
+    time.sleep(10)
 finally:
     driver.quit()
